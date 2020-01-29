09df1b4719a60a24b8aedf833d7db162f1c2f773
Lucas Sherwin <lksherwin@gmail.com>
2020-01-29 19:57:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,70 @@
 # My Code here....
+def map_to_negativize(arr)
+  neg_arr = []
+  i = 0
+  while i < arr.length do
+    neg_arr[i] = arr[i] * -1
+    i += 1
+  end
+  neg_arr
+end
+
+def map_to_no_change(arr)
+  new_arr = []
+  i = 0
+  while i < arr.length do
+    new_arr[i] = arr[i]
+    i += 1
+  end
+  new_arr
+end
+
+def map_to_double(arr)
+  double_arr = []
+  i = 0
+  while i < arr.length do
+    double_arr[i] = arr[i] * 2
+    i += 1
+  end
+  double_arr
+end
+
+def map_to_square(arr)
+  square_arr = []
+  i = 0
+  while i < arr.length do
+    square_arr[i] = arr[i] ** 2
+    i += 1
+  end
+  square_arr
+end
+
+def reduce_to_total(source_array, start = 0)
+  total = start
+  i = 0
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(arr)
+  val = true
+  i = 0
+  while i < arr.length do
+    val = false if !arr[i]
+    i += 1
+  end
+  val
+end
+
+def reduce_to_any_true(arr)
+  val = false
+  i = 0
+  while i < arr.length do
+    val = true if arr[i]
+    i += 1
+  end
+  val
+end

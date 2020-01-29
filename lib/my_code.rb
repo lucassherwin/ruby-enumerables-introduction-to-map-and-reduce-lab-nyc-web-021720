034cfb996e7aa2060123259a6aaec9fafd63d098
Lucas Sherwin <lksherwin@gmail.com>
2020-01-29 19:30:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,9 @@
 # My Code here....
 def map_to_negativize(arr)
-  {
-    neg_arr = []
-    i = 0
-    while i < arr.length do
-      neg_arr[i] = arr[i] * -1
-      i += 1
-    end
-    neg_arr
-  }
+  neg_arr = []
+  i = 0
+  while i < arr.length do
+    neg_arr[i] = arr[i] * -1
+    i += 1
+  end
+  neg_arr

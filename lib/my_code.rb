3e67b0df56960693ccd6bf0b399a13207cbbdb0a
Lucas Sherwin <lksherwin@gmail.com>
2020-01-29 19:39:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def map_to_square(arr)
   square_arr
 end
 
-def reduce_to_total(source_array, start_point)
+def reduce_to_total(source_array, start)
   reduce_arr = []
   i = start
   while i < source.length do
